70f8298520c6ecc1917334ea346b6a021b725942
Eslam Nabil <eslamnabilmohamed2@gmail.com>
2022-10-28 02:53:01 +0200

    modify some tables

--- app/Http/Controllers/Api/SectionTypeController.php
+++ app/Http/Controllers/Api/SectionTypeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\SectionType;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TypeResource;
 
 class SectionTypeController extends Controller
 {
