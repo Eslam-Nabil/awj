8158cc3f20d329d230c058edc3a2aa17e176c1e8
Eslam Nabil <eslamnabilmohamed2@gmail.com>
2022-11-10 08:14:47 +0200

    some updates

--- app/Models/articleTranslation.php
+++ app/Models/articleTranslation.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class articleTranslation extends Model
+class ArticleTranslation extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'article_translations';
     public $timestamps = false;
     protected $fillable = ['title', 'description'];
