fb5234fc38112b6d0a2ba31074867279e1346c20
@eslamnabilmohamed2 <eslamnabilmohamed2@gmail.com>
2023-03-19 21:24:28 +0200

    add image for article

---
+++ app/Http/Resources/ArticleOfAuthorResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleOfAuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[
+            'id'=>$this->id,
+            'serial_number'=>$this->serial_number,
+            'article_file_path'=>asset($this->article_file_path),
+            'description'=>$this->description,
+            'category_id'=>$this->category_id,
+            'price'=>$this->price,
+            'status'=>$this->status,
+            'pages_count'=>$this->pages_count,
+            'title'=>$this->title,
+            'user'=>$this->user->name,
+           ];
+    }
+}
