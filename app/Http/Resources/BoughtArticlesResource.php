addc24cfd03d4339c3c9da860c65aa3a9de023a5
@eslamnabilmohamed2 <eslamnabilmohamed2@gmail.com>
2023-03-19 21:21:21 +0200

    return certificate in resource

--- app/Http/Resources/BoughtArticlesResource.php
+++ app/Http/Resources/BoughtArticlesResource.php
@@ -31,7 +31,7 @@ class BoughtArticlesResource extends JsonResource
             'description'=>$this->description,
             'summary'=>$this->summary,
             'language'=>$this->language,
-            'certificate'=>$user->articles()->wherePivot('order_status','completed')->wherePivot('article_id',$this->id)->first()->certificate,
+            'certificate'=>$user->articles()->wherePivot('order_status','completed')->wherePivot('article_id',$this->id)->first()->pivot->certificate,
             'tasks'=>UserTaskResource::collection($user->tasks->where('article_id',$this->id)),
         ];
     }
